46afd7b6d1d73148c8e624e5f515ae579e705606
Hari25483 <haricodmw@gmail.com>
2022-09-28 01:03:09 +0530

    create next word tab

---
+++ grammerly_tamil2/lib/tabs/next_word.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class next_word extends StatelessWidget {
+  const next_word({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text("hello")));
+  }
+}
