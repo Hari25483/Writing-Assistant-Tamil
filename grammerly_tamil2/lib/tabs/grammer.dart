15d212898cfab065f04c9405365c42ce280de805
Hari25483 <haricodmw@gmail.com>
2022-09-28 01:37:17 +0530

    Update grammer.dart

--- grammerly_tamil2/lib/tabs/grammer.dart
+++ grammerly_tamil2/lib/tabs/grammer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
@@ -27,61 +28,103 @@ class _grammerState extends State<grammer> {
             const Divider(
               height: 100,
             ),
-            TextFormField(
-              autofocus: true,
-              obscureText: false,
-              onChanged: (value) => setState(() => text = value),
-              decoration: const InputDecoration(
-                hintText: 'Enter the text to check spelling\n',
-                hintStyle: TextStyle(fontSize: 20.0, color: Colors.grey),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0x00000000),
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(4.0),
-                    topRight: Radius.circular(4.0),
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0x00000000),
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(4.0),
-                    topRight: Radius.circular(4.0),
-                  ),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0x00000000),
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(4.0),
-                    topRight: Radius.circular(4.0),
-                  ),
-                ),
-                focusedErrorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0x00000000),
-                    width: 1,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(4.0),
-                    topRight: Radius.circular(4.0),
-                  ),
-                ),
+            RawKeyboardListener(
+              focusNode: FocusNode(),
+              onKey: (event) {
+                if (event.isKeyPressed(LogicalKeyboardKey.space)) {
+                  print("space pressed");
+                }
+              },
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter the text to check spelling'),
               ),
-              style: const TextStyle(fontSize: 20.0, color: Colors.black),
-              textAlign: TextAlign.center,
             ),
+            // TextFormField(
+            //   autofocus: true,
+            //   obscureText: false,
+            //   onChanged: (value) => setState(() => text = value),
+            //   decoration: const InputDecoration(
+            //     hintText: 'Enter the text to check spelling\n',
+            //     hintStyle: TextStyle(fontSize: 20.0, color: Colors.grey),
+            //     enabledBorder: UnderlineInputBorder(
+            //       borderSide: BorderSide(
+            //         color: Color(0x00000000),
+            //         width: 1,
+            //       ),
+            //       borderRadius: BorderRadius.only(
+            //         topLeft: Radius.circular(4.0),
+            //         topRight: Radius.circular(4.0),
+            //       ),
+            //     ),
+            //     focusedBorder: UnderlineInputBorder(
+            //       borderSide: BorderSide(
+            //         color: Color(0x00000000),
+            //         width: 1,
+            //       ),
+            //       borderRadius: BorderRadius.only(
+            //         topLeft: Radius.circular(4.0),
+            //         topRight: Radius.circular(4.0),
+            //       ),
+            //     ),
+            //     errorBorder: UnderlineInputBorder(
+            //       borderSide: BorderSide(
+            //         color: Color(0x00000000),
+            //         width: 1,
+            //       ),
+            //       borderRadius: BorderRadius.only(
+            //         topLeft: Radius.circular(4.0),
+            //         topRight: Radius.circular(4.0),
+            //       ),
+            //     ),
+            //     focusedErrorBorder: UnderlineInputBorder(
+            //       borderSide: BorderSide(
+            //         color: Color(0x00000000),
+            //         width: 1,
+            //       ),
+            //       borderRadius: BorderRadius.only(
+            //         topLeft: Radius.circular(4.0),
+            //         topRight: Radius.circular(4.0),
+            //       ),
+            //     ),
+            //   ),
+            //   style: const TextStyle(fontSize: 20.0, color: Colors.black),
+            //   textAlign: TextAlign.center,
+            // ),
             const Divider(
               height: 100,
             ),
-
+            // MaterialButton(
+            //     color: Colors.teal,
+            //     child: Text(
+            //       'Find',
+            //       style: TextStyle(color: Colors.white),
+            //     ),
+            //     onPressed: () async {
+            //       print('Pressed');
+            //       print(text);
+            //       // var url = Uri.https('https://api.genderize.io/?name=luc');
+            //       Response response = await get(
+            //           Uri.parse('http://35ec-35-231-30-146.ngrok.io/'));
+            //       // Await the http get response, then decode the json-formatted response.
+            //       // var response = await http.get(url);
+            //       print(response.toString());
+            //       if (response.statusCode == 200) {
+            //         var jsonResponse = convert.jsonDecode(response.body)
+            //             as Map<String, dynamic>;
+            //         var itemCount = jsonResponse['Hello'];
+            //         text1 = "$itemCount";
+            //         print(text1);
+            //         setState(() {});
+            //       } else {
+            //         print(
+            //             'Request failed with status: ${response.statusCode}.');
+            //       }
+            //
+            //       // print(await http.read(Uri.https('example.com', 'foobar.txt')));
+            //       // text = "Hello";),
+            //     }),
             TextButton(
               style: TextButton.styleFrom(
                 foregroundColor: Colors.teal,
