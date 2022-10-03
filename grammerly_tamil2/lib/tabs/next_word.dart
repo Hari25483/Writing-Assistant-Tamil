159e8f11a7cca25a770c09a7461f2b61ff803864
Hari25483 <haricodmw@gmail.com>
2022-10-04 01:23:26 +0530

    Call server once space is pressed for suggestions

--- grammerly_tamil2/lib/tabs/next_word.dart
+++ grammerly_tamil2/lib/tabs/next_word.dart
@@ -1,178 +1,174 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 import 'package:http/http.dart';
 
-class nextword extends StatefulWidget {
-  const nextword({Key? key}) : super(key: key);
+List<dynamic> spell_suggestions = [""];
+
+List<String> stringList = [""];
+TextEditingController inputTextController = TextEditingController();
+List<LogicalKeyboardKey> keys = [];
+String text = "";
+String text1 = "";
+
+class next_word extends StatefulWidget {
+  const next_word({Key? key}) : super(key: key);
 
   @override
-  State<nextword> createState() => _nextwordState();
+  State<next_word> createState() => _next_wordState();
 }
 
-class _nextwordState extends State<nextword> {
-  String text = "";
-  String text1 = "";
+class _next_wordState extends State<next_word> {
+  Future<void> call_api() async {
+    print("apicalled");
+    text = inputTextController.text;
+    print("api_text" + text);
+    Response response = await get(
+        Uri.parse('http://2507-34-86-28-150.ngrok.io/spelling?name=$text'));
+    print(response.toString());
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      // spell_suggestions = ;
+      /// for casting
+      stringList = jsonResponse.cast<String>();
+      print("String list" + stringList.toString());
+      print("api finished");
+
+      setState(() {});
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: InkWell(
-        onTap: () async {},
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            const Divider(
-              height: 100,
-            ),
-            RawKeyboardListener(
-              focusNode: FocusNode(),
-              onKey: (event) {
-                if (event.isKeyPressed(LogicalKeyboardKey.space)) {
-                  print("space pressed");
-                }
-              },
-              child: TextField(
-                decoration: InputDecoration(
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (event) async {
+        final key = event.logicalKey;
+        if (event is RawKeyDownEvent) {
+          if (keys.contains(key)) {
+            return;
+          }
+          if (event.isKeyPressed(LogicalKeyboardKey.space)) {
+            call_api();
+            print("after api " + stringList.toString());
+            setState(() {});
+          }
+          setState(() {
+            keys.add(key);
+          });
+        } else {
+          setState(() {
+            keys.remove(key);
+          });
+        }
+      },
+      child: Container(
+        child: SingleChildScrollView(
+          child: InkWell(
+            onTap: () async {},
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const Divider(
+                  height: 100,
+                ),
+                TextField(
+                  controller: inputTextController,
+                  decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    hintText: 'Enter the text to check spelling'),
-              ),
-            ),
-            // TextFormField(
-            //   autofocus: true,
-            //   obscureText: false,
-            //   onChanged: (value) => setState(() => text = value),
-            //   decoration: const InputDecoration(
-            //     hintText: 'Enter the text to check spelling\n',
-            //     hintStyle: TextStyle(fontSize: 20.0, color: Colors.grey),
-            //     enabledBorder: UnderlineInputBorder(
-            //       borderSide: BorderSide(
-            //         color: Color(0x00000000),
-            //         width: 1,
-            //       ),
-            //       borderRadius: BorderRadius.only(
-            //         topLeft: Radius.circular(4.0),
-            //         topRight: Radius.circular(4.0),
-            //       ),
-            //     ),
-            //     focusedBorder: UnderlineInputBorder(
-            //       borderSide: BorderSide(
-            //         color: Color(0x00000000),
-            //         width: 1,
-            //       ),
-            //       borderRadius: BorderRadius.only(
-            //         topLeft: Radius.circular(4.0),
-            //         topRight: Radius.circular(4.0),
-            //       ),
-            //     ),
-            //     errorBorder: UnderlineInputBorder(
-            //       borderSide: BorderSide(
-            //         color: Color(0x00000000),
-            //         width: 1,
-            //       ),
-            //       borderRadius: BorderRadius.only(
-            //         topLeft: Radius.circular(4.0),
-            //         topRight: Radius.circular(4.0),
-            //       ),
-            //     ),
-            //     focusedErrorBorder: UnderlineInputBorder(
-            //       borderSide: BorderSide(
-            //         color: Color(0x00000000),
-            //         width: 1,
-            //       ),
-            //       borderRadius: BorderRadius.only(
-            //         topLeft: Radius.circular(4.0),
-            //         topRight: Radius.circular(4.0),
-            //       ),
-            //     ),
-            //   ),
-            //   style: const TextStyle(fontSize: 20.0, color: Colors.black),
-            //   textAlign: TextAlign.center,
-            // ),
-            const Divider(
-              height: 100,
-            ),
-            // MaterialButton(
-            //     color: Colors.teal,
-            //     child: Text(
-            //       'Find',
-            //       style: TextStyle(color: Colors.white),
-            //     ),
-            //     onPressed: () async {
-            //       print('Pressed');
-            //       print(text);
-            //       // var url = Uri.https('https://api.genderize.io/?name=luc');
-            //       Response response = await get(
-            //           Uri.parse('http://35ec-35-231-30-146.ngrok.io/'));
-            //       // Await the http get response, then decode the json-formatted response.
-            //       // var response = await http.get(url);
-            //       print(response.toString());
-            //       if (response.statusCode == 200) {
-            //         var jsonResponse = convert.jsonDecode(response.body)
-            //             as Map<String, dynamic>;
-            //         var itemCount = jsonResponse['Hello'];
-            //         text1 = "$itemCount";
-            //         print(text1);
-            //         setState(() {});
-            //       } else {
-            //         print(
-            //             'Request failed with status: ${response.statusCode}.');
-            //       }
-            //
-            //       // print(await http.read(Uri.https('example.com', 'foobar.txt')));
-            //       // text = "Hello";),
-            //     }),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.teal,
-                disabledForegroundColor: Colors.yellow.withOpacity(0.38),
-                side: BorderSide(color: Colors.teal, width: 2),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(25))),
-              ),
-              onPressed: () async {
-                print('Pressed');
-                print(text);
-                // var url = Uri.https('https://api.genderize.io/?name=luc');
-                Response response =
-                    await get(Uri.parse('http://35ec-35-231-30-146.ngrok.io/'));
-                // Await the http get response, then decode the json-formatted response.
-                // var response = await http.get(url);
-                print(response.toString());
-                if (response.statusCode == 200) {
-                  var jsonResponse =
-                      convert.jsonDecode(response.body) as Map<String, dynamic>;
-                  var itemCount = jsonResponse['Hello'];
-                  text1 = "$itemCount";
-                  print(text1);
-                  setState(() {});
-                } else {
-                  print('Request failed with status: ${response.statusCode}.');
-                }
+                    hintText: 'Enter the text to check spelling',
+                  ),
+                ),
 
-                // print(await http.read(Uri.https('example.com', 'foobar.txt')));
-                // text = "Hello";
-              },
-              child: const Padding(
-                padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                child: Text('Find',
-                    style: TextStyle(
-                        color: Colors.teal,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500)),
-              ),
-            ),
+                const Divider(
+                  height: 100,
+                ),
 
-            const Divider(
-              height: 50,
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.teal,
+                    disabledForegroundColor: Colors.yellow.withOpacity(0.38),
+                    side: BorderSide(color: Colors.teal, width: 2),
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25))),
+                  ),
+                  onPressed: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: Text('Find',
+                        style: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500)),
+                  ),
+                ),
+// Generated code for this Button Widget..
+                const Divider(
+                  height: 20,
+                ),
+
+                // Container(
+                //   margin: const EdgeInsets.all(15.0),
+                //   padding: const EdgeInsets.all(3.0),
+                //   width: 300,
+                //   height: 50,
+                //   decoration:
+                //       BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                //   child: Text(
+                //     text1,
+                //     textAlign: TextAlign.center,
+                //   ),
+                // ),
+                const DropdownButtonExample()
+              ],
             ),
-            Text(text1), // display your text
-          ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = stringList.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: null,
+      // icon: const Icon(Icons.arrow_downward),
+      iconEnabledColor: Colors.green,
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+          inputTextController.text = "$text $dropdownValue";
+        });
+      },
+      items: stringList.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
